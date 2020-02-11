09fde764d780192b9d5133720a5d2a624e294816
MiracleRo <741556866@qq.com>
2020-02-11 15:21:54 +0800

    修改了db的host

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 const chalk = require('chalk')
 
 const sequelize = new Sequelize('mock_api', 'root', 'qwerty!@#', {
-  host: 'localhost',
+  host: 'db',
   dialect: 'mysql'
 });
 
@@ -13,7 +13,8 @@ sequelize
     console.log(chalk.green(`连接数据库成功!`))
   })
   .catch(err => {
-    console.log(chalk.red(`连接数据库失败: ${err}`))
+    console.log(err)
+    console.log(chalk.red(`连接数据库失败:`))
   });
 
 module.exports = sequelize
